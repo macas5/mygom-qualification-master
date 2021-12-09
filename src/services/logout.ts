f4bf80c75fcd614b1a38a2d58db8704cd706a2e5
macas5 <mkalinauskas1993@gmail.com>
2021-12-09 10:31:41 +0000

    Refactored code

--- src/services/logout.ts
+++ src/services/logout.ts
@@ -3,15 +3,12 @@ import getUrl from '../utils/getUrl';
 
 const logout = async (token: string) => {
   const url = getUrl(API.Logout);
-  
-
   const response = await fetch(url, {
     method: 'get',
     headers: new Headers({
       'Authorization': 'Bearer '+token
     })
   });
-
   localStorage.removeItem('token');
 };
 
--- src/services/updateItem.ts
+++ src/services/updateItem.ts
@@ -3,14 +3,14 @@ import getUrl from "~/utils/getUrl";
 import { IItem } from "./getUserItems";
 
 const updateItem = (item: IItem) => (
-    fetch(getUrl(API.Items), {
-        method: "POST",
-        body: JSON.stringify(item),
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-        }
-    })
+	fetch(getUrl(API.Items), {
+		method: "POST",
+		body: JSON.stringify(item),
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${localStorage.getItem('token')}`,
+		}
+	})
 )
 
 export default updateItem;
